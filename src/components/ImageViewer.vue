c55fd5e7671ecd97d732622b517697047972bb66
Julien Rousset <julien.r@geronimostudios.com>
2020-01-28 00:00:43 +0100

    [Lint] Lint new file

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -1,71 +1,72 @@
 <template>
 	<div class="image-viewer" ref="imgviewer">
-        <div class="inner-image-viewer">
-            <img :src="image" :style="{ top: offset + 'px' }" ref="img">
-        </div>
-    </div>
+		<div class="inner-image-viewer">
+			<img :src="image" :style="{ top: offset + 'px' }" ref="img">
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+	import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
 
-@Component({
-	name: 'ImageViewerComponent'
-})
-export default class ImageViewerComponent extends Vue {
-    public offset = 0;
-    private bindedHandleScroll!: (event: WheelEvent) => void;
-    private bindedHandleResize!: () => void;
-    private maxOffset = 0;
+	@Component({
+		name: 'ImageViewerComponent'
+	})
+	export default class ImageViewerComponent extends Vue {
+		public offset = 0;
 
-    @Prop({ required: true })
-    public image!: string;
+		@Prop({ required: true })
+		public image!: string;
 
-    public beforeMount () {
-        this.bindedHandleScroll = this.handleScroll.bind(this);
-        this.bindedHandleResize = this.initScroll.bind(this);
-    }
+		private bindedHandleScroll!: (event: WheelEvent) => void;
+		private bindedHandleResize!: () => void;
+		private maxOffset = 0;
 
-    public mounted () {
-        const ref = this.$refs.imgviewer as HTMLElement;
-        ref.addEventListener('wheel', this.bindedHandleScroll);
-        window.addEventListener('resize', this.bindedHandleResize);
-        const img = this.$refs.img as HTMLImageElement;
-        this.maxOffset = img.height - ref.offsetHeight;
-        this.initScroll();
-    }
+		public beforeMount () {
+			this.bindedHandleScroll = this.handleScroll.bind(this);
+			this.bindedHandleResize = this.initScroll.bind(this);
+		}
 
-    @Watch('image')
-    public initScroll () {
-        const ref = this.$refs.imgviewer as HTMLElement;
-        const img = this.$refs.img as HTMLImageElement;
+		public mounted () {
+			const ref = this.$refs.imgviewer as HTMLElement;
+			ref.addEventListener('wheel', this.bindedHandleScroll);
+			window.addEventListener('resize', this.bindedHandleResize);
+			const img = this.$refs.img as HTMLImageElement;
+			this.maxOffset = img.height - ref.offsetHeight;
+			this.initScroll();
+		}
 
-        const tmpImg = new Image();
-        tmpImg.addEventListener('load', () => {
-            this.maxOffset = img.height - ref.offsetHeight;
-            this.checkBoundaries();
-        });
-        tmpImg.src = img.src;
-    }
+		@Watch('image')
+		public initScroll () {
+			const ref = this.$refs.imgviewer as HTMLElement;
+			const img = this.$refs.img as HTMLImageElement;
 
-    public beforeDestroy () {
-        const ref = this.$refs.imgviewer as HTMLDivElement;
-        ref.removeEventListener('wheel', this.bindedHandleScroll);
-        window.removeEventListener('resize', this.bindedHandleResize);
-    }
+			const tmpImg = new Image();
+			tmpImg.addEventListener('load', () => {
+				this.maxOffset = img.height - ref.offsetHeight;
+				this.checkBoundaries();
+			});
+			tmpImg.src = img.src;
+		}
 
-    private handleScroll (event: WheelEvent) {
-        event.preventDefault();
-        this.offset -= event.deltaY;
-        this.checkBoundaries();        
-    }
+		public beforeDestroy () {
+			const ref = this.$refs.imgviewer as HTMLDivElement;
+			ref.removeEventListener('wheel', this.bindedHandleScroll);
+			window.removeEventListener('resize', this.bindedHandleResize);
+		}
 
-    private checkBoundaries () {
-        if (this.offset > 0) {
-            this.offset = 0;
-        } else if (this.offset < -this.maxOffset) {
-            this.offset = -this.maxOffset;
-        }
-    }
-}
+		private handleScroll (event: WheelEvent) {
+			event.preventDefault();
+			this.offset -= event.deltaY;
+			this.checkBoundaries();
+		}
+
+		private checkBoundaries () {
+			if (this.offset > 0) {
+				this.offset = 0;
+			} else if (this.offset < -this.maxOffset) {
+				this.offset = -this.maxOffset;
+			}
+		}
+	}
 </script>
